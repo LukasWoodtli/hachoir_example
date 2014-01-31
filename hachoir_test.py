11f1fc344145a9fb371b2e4dfebff071fbfb3041
woodtluk <woodtluk@e17a30e0-a4a0-48a1-9280-7d2f855b2862>
2014-01-31 19:08:00 +0000

    Hachoir Beispiel commited.
    git-svn-id: https://xp-dev.com/svn/woodtluk-myStuff@622 e17a30e0-a4a0-48a1-9280-7d2f855b2862

--- hachoir_test.py
+++ hachoir_test.py
@@ -1,5 +1,49 @@
-from hachoir_parser import parser
+__author__ = 'Boot'
+
+
+import hachoir_parser
+from hachoir_core.cmd_line import unicodeFilename
+from hachoir_core.tools import makePrintable
+from hachoir_metadata import extractMetadata
+from hachoir_core.i18n import getTerminalCharset
+from hachoir_core.field import GenericFieldSet
 
 
 
 
+
+
+def print_metadata(metadata):
+    text = metadata.exportPlaintext()
+    charset = getTerminalCharset()
+    for line in text:
+     pass #   print makePrintable(line, charset)
+
+    # from http://stackoverflow.com/questions/14546533/hachoir-retrieving-data-from-a-group
+    # See what keys you can extract
+    for k,v in metadata._Metadata__data.iteritems():
+        if v.values:
+            print v.key, v.values[0].value
+
+#print type(p)
+def print_recursively(fields):
+    f = fields.iteritems()
+    for k,v in f:
+        print "%s=%s" % (k, v)
+        if isinstance(v, GenericFieldSet):
+            print_recursively(v._fields)
+
+
+def main(filename = "default_64.png"):
+    filename, realname = unicodeFilename(filename), filename
+
+    p = hachoir_parser.createParser(filename, realname)
+    metadata = extractMetadata(p)
+    print_metadata(metadata)
+
+    fields = p._fields
+    print_recursively(fields)
+
+
+if __name__ == "__main__":
+    main()
